edfa7d1b9fc83a643f3d48ec7eb9de39af7539e5
joaovtw <joao.wanderley@ccc.ufcg.edu.br>
2019-11-13 17:21:11 -0300

    melhorias na view inicial

--- ajude.js
+++ ajude.js
@@ -1,4 +1,5 @@
 (async function main() {
+  let data = await Promise.all([fetch_templates()]);
 
   let hash = location.hash;
   if ([""].includes(hash)) {
@@ -6,8 +7,24 @@
   }
 }());
 
+let templateInicial;
+async function fetch_templates() {
+  let html_templates = await (fetch('templates.html').then(r => r.text()));
+  let e = document.createElement("div");
+  e.innerHTML = html_templates;
+  templateInicial = e.querySelector('#viewInicial');
+}
+
 function viewInicial() {
-  let $template = document.querySelector('#viewInicial');
+  let $viewer = document.querySelector('#viewer');
+  let $template = templateInicial;
   $viewer.innerHTML = $template.innerHTML;
+
+  let $mensagemInicial = document.querySelector('#mensagemInicial');
+  $mensagemInicial.innerText = "Bem vindo";
+
+  let $login = document.querySelector('#login');
+  $mensagemInicial.innerText = "Bem vindo";
+
 
 }
