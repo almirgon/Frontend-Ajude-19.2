c64ae8bbf5aba14521c9099d08271a24267b784b
Almir <almir.crispiniano@ccc.ufcg.edu.br>
2019-11-25 01:30:06 -0300

    Conclusao do front

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var path = require('path');
 var app = express()
+var cors = require('cors')
 app.use(express.static(__dirname))
-app.get('*', (req, res) => {
+app.use(cors())
+app.get('/', (req, res) => {
      res.sendfile(path.resolve('index.html'))
     })
 app.listen(process.env.PORT || 8000);
