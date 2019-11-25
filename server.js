d5f11e69e48e2c88c42b7db4fa07b580801c29ad
Almir <almir.crispiniano@ccc.ufcg.edu.br>
2019-11-24 22:55:45 -0300

    Conclusao do front

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var express = require('express')
-var app = require('express')()
-app.use(express.static('app'))
+var app = express()
+//app.use(express.static('app'))
+app.get('*', (req, res) => {
+     res.sendfile(__dirname + '/index.html')
+     res.writeHead(200, {'Content-Type': 'text/javascript'}); 
+    })
 app.listen(process.env.PORT || 8000);
+
