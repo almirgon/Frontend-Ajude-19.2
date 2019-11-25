e9383989ab0f6235a0f03f51f17b23f0dec47ee2
Almir <almir.crispiniano@ccc.ufcg.edu.br>
2019-11-24 22:22:29 -0300

    Conclusao do front

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 var express = require('express')
 var app = require('express')()
 app.use(express.static('app'))
-app.get("/",(req, res) =>{
-    res.redirect("/#/home")
-})
 app.listen(process.env.PORT || 8000);
