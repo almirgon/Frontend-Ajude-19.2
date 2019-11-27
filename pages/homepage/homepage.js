c89d60f6160c792c6790b3233b4ccb8b215b8b53
joaovtw <joao.wanderley@ccc.ufcg.edu.br>
2019-11-27 00:36:02 -0300

    homepage feito

--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -3,7 +3,7 @@ import CampaignService from "../../services/campaign/CampaignService.js";
 let HomePage = {
     render: async () => /*html*/ `
     <main class="wrap">
-       <nav-bar id="nav-bar"></nav-bar>
+       <header-menu id="header-menu"></header-menu>
        <h1>Pesquise sua campanha</h1>
        <renderCampaign id="renderCampaign"></renderCampaign>
        <h1>Rankear campanhas por:</h1>
@@ -15,10 +15,41 @@ let HomePage = {
        <section id="campaignsHome"></section>
     </main>
     `, after_render: async() => {
+        const $campaignsHome = document.getElementById("campaignsHome");
+
         let $rank = document.getElementById("rank");
         let chosen = $rank.options[$rank.selectedIndex].innerHTML;
 
-        let campaings = CampaignService.getAllCampaings();
+        let list = CampaignService.getAllCampaings();
+        let sortable;
+
+        if(chosen == 'meta'){
+            sortable = Object.keys(list).sort(function(a,b){return list[a.goal]-list[b.goal]});
+        } else if (chosen == 'likes') {
+            sortable = Object.keys(list).sort(function(a,b){return list[a.numberLikes]-list[b.numberLikes]});
+        } else {
+            sortable = Object.keys(list).sort(function(a,b){return list[a.date]-list[b.date]});
+        }
+
+        let chosens = [];
+
+        for (i = 0; (i < 5) && (sortable.length > 0); i++) {
+            chosens[i] = sortable[i];
+          }
+
+          for (let campaign in chosens) {
+            const campaignCard = document.createElement('div')
+            const paragraph = document.createElement("p")
+            const anchor = document.createElement("a")
+            anchor.setAttribute("href", `/#/campaign/${campaign.url}`)
+            paragraph.innerHTML = campaign.name + " | Faltam: R$ " + (campaign.goal - campaign.raisedAmount) + " | Likes: " +campaign.numberLikes
+            anchor.appendChild(paragraph)
+            campaignCard.appendChild(anchor)
+            $campaignsHome.appendChild(campaignCard)
+         }
+
+
+
         
 
     }
